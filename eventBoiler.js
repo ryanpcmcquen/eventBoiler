bd93b40243b3800ada2cd331685c2630ebc0b15d
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-01-07 15:44:12 -0800

    Change email.

--- eventBoiler.js
+++ eventBoiler.js
@@ -3,7 +3,7 @@
 /* eventBoiler v0.2.2 by @ryanpcmcquen */
 // https://github.com/ryanpcmcquen/eventBoiler
 //
-// Ryan P.C. McQuen | Everett, WA | ryan.q@linux.com
+// Ryan P.C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
